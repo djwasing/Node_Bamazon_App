5a3e2b6fec91037ce5fe9d9da91be952776bc4da
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-02-24 20:38:33 -0600

    cleaning

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,8 +14,7 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    listAll(); // need to find a better way to show this data so it looks nicer, maybe JSON?
-    //selectProduct();
+    listAll(); 
 });
 
 function listAll() {
